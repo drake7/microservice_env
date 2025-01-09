6c79910ca0476ef2706b765dbafead7d8ef72665
drake7 <deepak.kumar7v@gmail.com>
2025-01-08 22:50:02 -0800

    Open telemetry POC

---
+++ openTelemetryPOC/src/test/java/com/example/openTelemetryPOC/OpenTelemetryPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openTelemetryPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTelemetryPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
